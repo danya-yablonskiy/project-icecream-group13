9e81f2d5f2e048a70caa8c98ac005a3ff418a459
VitaliiMaC9m6uk <mac9m6uk@gmail.com>
2022-12-24 15:19:56 +0100

    swiper

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,30 @@
-// const { default: Swiper } = require("swiper");
+import Swiper from "swiper";
 
 //Ініціалізація Swiper
-new Swiper('.swiper');
+new Swiper('.swiper-1',{
+    //безкінченність
+    loop: true,
+    //Переключення при кліке
+    slideToClickedSlide: true,
+    //автопрокрутка
+    speed: 2000,
+    spaceBetween: 100 ,
+    centeredSlides: true,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+        // renderBullet: function (index, className) {
+        //     return '<li class="' + className + '"></li>';
+        // },
+    }
+    
+    
+});
+
+new Swiper('.swiper-2', {
+    pagination: {
+        el: '.swiper-pagination',
+        type:"bullets",
+        clickable: true
+    }
+});
