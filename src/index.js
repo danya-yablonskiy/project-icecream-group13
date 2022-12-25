1386b6d977d946330943a6198b29b3accc6bc6d5
AleksandrIvantsov <7022713@gmail.com>
2022-12-25 15:23:48 +0200

    Додав скрипт відкриття модального вікна

--- src/index.js
+++ src/index.js
@@ -27,3 +27,18 @@
     refs.modal.classList.toggle('header-backdrop--is-hidden');
   }
 })();
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open1]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('header-backdrop--is-hidden');
+  }
+})();
