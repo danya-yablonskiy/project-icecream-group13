96e4c10008dc0e36119b4a9a6e0fdcd4a60889d2
AleksandrIvantsov <7022713@gmail.com>
2022-12-25 21:51:20 +0200

    Дрібні доробки

--- src/index.js
+++ src/index.js
@@ -15,27 +15,12 @@
 
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
+    openModalBtns: document.querySelectorAll('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
-
-  function toggleModal() {
-    refs.modal.classList.toggle('header-backdrop--is-hidden');
-  }
-})();
-
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector('[data-modal-open1]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
-  };
-
-  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtns.forEach(btn => btn.addEventListener('click', toggleModal));
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
