919f7ad2a485948c52c33d34c0e13042ad62e3d9
AleksandrIvantsov <7022713@gmail.com>
2022-12-25 03:36:44 +0200

    Перша версія modal-buy-now

---
+++ src/index.js
@@ -0,0 +1,32 @@
+<<<<<<< Updated upstream
+=======
+(() => {
+  const menuBtnRef = document.querySelector('[data-menu-button]');
+  const mobileMenuRef = document.querySelector('[data-menu]');
+
+  menuBtnRef.addEventListener('click', () => {
+    const expanded =
+      menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
+
+    menuBtnRef.classList.toggle('menu-button--is-open');
+    menuBtnRef.setAttribute('aria-expanded', !expanded);
+
+    mobileMenuRef.classList.toggle('main-nav__wrapper--is-open');
+  });
+})();
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('header-backdrop--is-hidden');
+  }
+})();
+>>>>>>> Stashed changes
