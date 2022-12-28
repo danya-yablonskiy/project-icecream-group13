78791c21b208e0bfc791442a7c40d5d140500306
danya-yablonskiy <115042288+danya-yablonskiy@users.noreply.github.com>
2022-12-28 18:55:10 +0000

    Deploying to gh-pages from @ danya-yablonskiy/project-icecream-group13@eb2e5f6fee902c981415ebaf632cd3a3975bb063 🚀

---
+++ index.11e43766.js
@@ -0,0 +1,2 @@
+!function(){var o={openModalBtn:document.querySelector("[data-modal-locations-open]"),closeModalBtn:document.querySelector("[data-modal-locations-close]"),modal:document.querySelector("[data-modal-locations]")};function e(){o.modal.classList.toggle("is-hidden")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
+//# sourceMappingURL=index.11e43766.js.map
---
+++ index.3be83f9b.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[data-modal-franchise-open]"),closeModalBtn:document.querySelector("[data-modal-franchise-close]"),modal:document.querySelector("[data-modal-franchise]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.3be83f9b.js.map
